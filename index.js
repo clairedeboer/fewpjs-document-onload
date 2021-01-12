f0bd3aa83eee8b90f6f99cec648c26edcc7fc115
clairedeboer <claire.deboer@gmail.com>
2021-01-12 18:34:51 -0500

    commit tree f20597396347b3633182936b40a3340d10928128

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+  updateDOM();
+}); 
+
+const updateDOM = () => {
+  document.querySelector("#text").innerHTML = "This is really cool!"
+}
